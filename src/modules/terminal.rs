0d68a822281b3fde7db2f76de8f95232d315f47c
ra1nxyz <ov3rsights@protonmail.com>
2026-06-07 23:43:42 +0100

    refactor method for ppid, comm fetch and add initial method for terminal fetch with traversing process tree

--- src/modules/terminal.rs
+++ src/modules/terminal.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::path::Path;
 
 #[derive(Debug)]
 pub struct Terminal {
@@ -9,38 +10,79 @@ pub struct Terminal {
 }
 
 pub fn get_terminal_info() -> Terminal {
-
     let proc_shell = get_proc_shell_by_pid()
         .or_else(|| get_user_shell_by_env());
 
-   return Terminal {
-        terminal_name: Some(String::from("test")), // get terminal later im hungry
+   Terminal {
+        terminal_name: attempt_get_terminal(),
         terminal_type: get_terminal_type_by_env(),
         user_shell: get_user_shell_by_env(),
         proc_shell: proc_shell,
    }
 }
 
-fn get_proc_shell_by_pid() -> Option<String> {
-    let parent_id_read_buf = fs::read_to_string("/proc/self/stat").ok()?; // i think theres a 
-    let parent_id = parent_id_read_buf.split_whitespace().nth(3)?;        // better way of doing
-                                                                          // this, but just making
-                                                                          // read as a buffer so
-                                                                          // the value doesnt get
-                                                                          // dropped too fast is
-                                                                          // wtv 
+fn get_parent_process_id(process_id: u32) -> Option<u32> {
+    let stat = fs::read_to_string(format!("/proc/{process_id}/stat")).ok()?;
+    let format = stat.split_once(") ")?.1;
+    format.split_whitespace().nth(1)?.parse().ok()
+}
 
-    let shell = fs::read_to_string(format!("/proc/{}/comm", parent_id)).ok()?;
-    return Some(shell.trim().to_string());
+fn get_comm_out(process_id: u32) -> Option<String> {
+    fs::read_to_string(format!("/proc/{process_id}/comm"))
+        .ok()
+        .map(|s| s.trim().to_string())
+}
+
+
+
+fn attempt_get_terminal() -> Option<String> {
+
+    const KNOWN_TERMINALS: &[&str] = &[  // adjust or use different method for actually storing
+                                         // these
+        "kitty",
+        "wezterm-gui",
+        "alacritty",
+        "ghostty",
+        "konsole",
+        "gnome-terminal-server",
+        "xfce4-terminal",
+        "foot",
+        "xterm",
+        "terminator",
+        "tilix",
+    ];
+    let mut process_id = std::process::id();
+
+    while let Some(parent_id) = get_parent_process_id(process_id) {
+        let comm = get_comm_out(parent_id)?;
+
+        if KNOWN_TERMINALS.contains(&comm.as_str()) {
+            return Some(comm);
+        }
+
+        if parent_id <= 1 {
+            break;
+        }
+        process_id = parent_id;
+    }
+    None
+}
+
+fn get_proc_shell_by_pid() -> Option<String> {
+    let process_id = std::process::id();
+    let parent_id = get_parent_process_id(process_id)?;
+
+    get_comm_out(parent_id)
 }
 
 fn get_user_shell_by_env() -> Option<String> {
-    return std::env::var("SHELL")
-        .ok()
-        .filter(|v| !v.is_empty());         // only gets user shell not what is running on the
-                                            // terminal where program is ran, and may also be
-                                            // incorrect if shell is changed within terminal, pid
-                                            // for accuracy, acts as fallback
+    let shell = std::env::var("SHELL").ok()?;
+
+    Some(Path::new(&shell)
+        .file_name()?
+        .to_string_lossy()
+        .into_owned()           
+    )
 }
 
 fn get_terminal_type_by_env() -> Option<String> {
